f5a7535a2e65fdeed711d2394db6890bf310730c
Rdelfito <rdelfitobr@gmail.com>
2024-08-08 10:22:31 -0300

    regra de boleto

--- src/Application.java
+++ src/Application.java
@@ -2,5 +2,6 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Transference");
         System.out.println("Configuration banco de dados");
+        System.out.println("reloads de engagement de boleto");
     }
 }
